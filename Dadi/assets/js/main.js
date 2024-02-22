811966b07cfd0f90e190f883754a033f4db4d344
fraberret <f.berretta02@gmail.com>
2024-02-22 16:08:47 +0100

    add program ("Dadi" exercise)

--- Dadi/assets/js/main.js
+++ Dadi/assets/js/main.js
@@ -1,1 +1,19 @@
-console.log('it works');
+
+
+let numbers = [1,2,3,4,5,6]
+
+
+let random1 = numbers[Math.floor(Math.random()*numbers.length)]
+
+console.log(`User 1: ${random1}`);
+
+let random2 = numbers[Math.floor(Math.random()*numbers.length)]
+
+console.log(`Computer: ${random2}`);;
+
+if (random1>random2) {
+    console.log('User 1 vince' );
+    
+} else{
+    console.log('Computer vince');
+}
