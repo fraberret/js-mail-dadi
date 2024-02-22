1cedf67ec5ef0d02795defd305a785f1b5f66470
fraberret <f.berretta02@gmail.com>
2024-02-22 13:55:01 +0100

    add cicle for ("Dadi" exercise)

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -1,12 +1,17 @@
 console.log("it works");
 
 //creare un array che contiene varie mail
-let mails=['pinco.pallino@gmail.com' , 'pallino.pinco@gmail.com' , 'pippo.paperino@gmail.com' ]
+let emails=['pinco.pallino@gmail.com' , 'pallino.pinco@gmail.com' , 'pippo.paperino@gmail.com' ]
 //creare una costante per la mail che inserisce l'utente
 let userMail= 'pinco.pallino@gmail.com'
 //creare una costante che indica mail non trovata
 let find = 'Non trovata'
 //creare un ciclo 
+for (let i = 0; i < emails.length; i++) {
+    const email = emails[i];
+    console.log(email);
+    
+}
 //creare SE mail=trovata stampa "mail trovata"
 //stampa i risultati
 console.log(find);
