2a661ef65d3015e97745c4216c60cb78113fc1b5
fraberret <f.berretta02@gmail.com>
2024-02-22 13:52:57 +0100

    add variables ("Dadi" exercise)

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -1,1 +1,12 @@
 console.log("it works");
+
+//creare un array che contiene varie mail
+let mails=['pinco.pallino@gmail.com' , 'pallino.pinco@gmail.com' , 'pippo.paperino@gmail.com' ]
+//creare una costante per la mail che inserisce l'utente
+let userMail= 'pinco.pallino@gmail.com'
+//creare una costante che indica mail non trovata
+let find = 'Non trovata'
+//creare un ciclo 
+//creare SE mail=trovata stampa "mail trovata"
+//stampa i risultati
+console.log(find);
