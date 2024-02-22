7a928bb488f44b1c8390502d0be44bd305c2c727
fraberret <f.berretta02@gmail.com>
2024-02-22 14:29:50 +0100

    take email and submit by form ("Mail exercise"

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -2,15 +2,22 @@ console.log("it works");
 
 //creare un array che contiene varie mail
 let emails=['pinco.pallino@gmail.com' , 'pallino.pinco@gmail.com' , 'pippo.paperino@gmail.com' ]
-//creare una costante per la mail che inserisce l'utente
-let userMail= 'pippo.paperino@gmail.com'
-console.log(userMail);
-//creare una costante che indica mail non trovata
-let find = 'Non trovata'
-//creare un ciclo 
-for (let i = 0; i < emails.length; i++) {
+
+
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    //creare una costante per la mail che inserisce l'utente
+    let userMail= document.getElementById('email').value
+    
+    //creare una costante che indica mail non trovata
+    let find = 'Non trovata'
+    
+    //creare un ciclo 
+    for (let i = 0; i < emails.length; i++) {
     let email = emails[i];
     console.log(email);
+    
     //creare SE mail=trovata stampa "mail trovata"
     if (email==userMail) {
         find="Trovata"
@@ -20,3 +27,5 @@ for (let i = 0; i < emails.length; i++) {
 }
 //stampa i risultati
 console.log(find);
+})
+
