58b1d41b857bf218148ee2e3a7e2aafac49afc4a
fraberret <f.berretta02@gmail.com>
2024-02-22 17:13:46 +0100

    add interface ("Dadi" exercise)

--- Dadi/assets/js/main.js
+++ Dadi/assets/js/main.js
@@ -1,24 +1,33 @@
 
-
 let numbers = [1,2,3,4,5,6]
-
-
+let user1Number = document.getElementById('user1Number')
+let computerNumber = document.getElementById('computerNumber')
 let random1 = numbers[Math.floor(Math.random()*numbers.length)]
-
-console.log(`User 1: ${random1}`);
-
 let random2 = numbers[Math.floor(Math.random()*numbers.length)]
 
-console.log(`Computer: ${random2}`);;
+let results=document.getElementById('result')
+
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault()
+
+    user1Number.innerHTML=random1
+    computerNumber.innerHTML=random2
 
 if (random1>random2) {
     console.log('User 1 vince' );
+    results.innerHTML='User 1 vince'
     
-    
-} else if (random1=random2) {
+} else if (random1==random2) {
     console.log('Parità');
+    results.innerHTML='Parità'
     
 
 }else{
     console.log('Computer vince');
+    results.innerHTML='Computer vince'
 }
+
+})
+
+
+
